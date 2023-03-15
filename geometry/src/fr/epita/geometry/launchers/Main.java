ffcd88ce607324c1cabd6cca652b184d4b52f608
Thomas <thomas.broussard@gmail.com>
2023-03-15 15:11:52 +0100

    * geometry tutorial

--- geometry/src/fr/epita/geometry/launchers/Main.java
+++ geometry/src/fr/epita/geometry/launchers/Main.java
@@ -1,7 +1,12 @@
 package fr.epita.geometry.launchers;
 
+import fr.epita.geometry.datamodel.Triangle;
+
 public class Main {
     public static void main(String[] args) {
+        Triangle t = new Triangle(2.0, 3.0, 4.0, 5.0);
+
+        System.out.println("area of triangle :" + t.getArea());
 
     }
 }
