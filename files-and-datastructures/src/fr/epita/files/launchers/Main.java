7dc5f40252dd702e12efa723b04b3a701df313e3
User <thomas.broussard@gmail.com>
2023-03-22 15:40:03 +0100

    * add file write operations

--- files-and-datastructures/src/fr/epita/files/launchers/Main.java
+++ files-and-datastructures/src/fr/epita/files/launchers/Main.java
@@ -5,24 +5,30 @@ import fr.epita.files.services.MyLogger;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.nio.file.Path;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        writeTests();
+
+        //2014/05/31 - 10:06:14.795 : fr.tbr.iamcore.Main.main()[19] : [INFO] Beginning of the program
+        List<String> strings = Files.readAllLines(Path.of("./test.csv"));
+        strings.remove(0);
+        for (String s : strings){
+            System.out.println(s);
+        }
+    }
+
+    private static void writeTests() {
         File wd = new File(".");
         System.out.println(wd.getAbsolutePath());
 //        outputToLog("./test.csv", "test,from,file\n");
 
 
-
         //outputToLog("./my.log", logLine);
         MyLogger testLogger = new MyLogger("./my.log", Main.class.getName());
         testLogger.info("test from main!");
-
-        //2014/05/31 - 10:06:14.795 : fr.tbr.iamcore.Main.main()[19] : [INFO] Beginning of the program
-        Files.rea
     }
 
 
