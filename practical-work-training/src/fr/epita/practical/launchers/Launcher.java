228d3f143645e406e0c19bc8e2aec0c7407bb0d6
User <thomas.broussard@gmail.com>
2023-03-22 16:49:34 +0100

    * practical work training

---
+++ practical-work-training/src/fr/epita/practical/launchers/Launcher.java
@@ -0,0 +1,11 @@
+package fr.epita.practical.launchers;
+
+
+import fr.epita.practical.tests.TestDMO2;
+
+public class Launcher {
+    public static void main(String[] args) {
+        TestDMO2.test();
+
+    }
+}
