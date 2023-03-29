9e10151d4e0b79d3f61f04e161e6f5a02a86556e
Présentation <tbr@tbr.com>
2023-03-29 15:18:11 +0200

    * fetching from databases

--- jdbc-examples/src/Main.java
+++ jdbc-examples/src/Main.java
@@ -1,31 +1,47 @@
 import java.sql.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
 
         System.out.println("Hello world!");
+        Connection connection = connect();
 
-        Connection connection = DriverManager.getConnection("jdbc:h2:mem:default");
+        ensureDDL(connection);
+
+        saveDoctor(connection, 1, "thomas");
+
+        saveDoctor(connection, 2, "quentin");
+
+        searchDoctors(connection);
+
+        connection.close();
 
 
-        PreparedStatement preparedStatement = connection.prepareStatement("CREATE TABLE DOCTOR(id INT NOT NULL, name VARCHAR(255))");
-        preparedStatement.execute();
+    }
 
-        connection.prepareStatement("INSERT INTO DOCTOR(id,name) values (1, 'Thomas')")
-            .execute();
-
-
-        connection.prepareStatement("INSERT INTO DOCTOR(id,name) values (1, 'Quentin')").execute();
-
+    private static List searchDoctors(Connection connection) throws SQLException {
         PreparedStatement selectStatement = connection.prepareStatement("SELECT name FROM DOCTOR");
         ResultSet resultSet = selectStatement.executeQuery();
         while(resultSet.next()){
             String name = resultSet.getString("name");
             System.out.println(name);
         }
+        return null; //todo fix that
+    }
 
-        connection.close();
+    private static void saveDoctor(Connection connection, Integer id, String name) throws SQLException {
+        connection.prepareStatement("INSERT INTO DOCTOR(id,name) values (" + id + ", " + name + ")")
+            .execute();
+    }
 
+    /** ensures the database is correctly initialized**/
+    private static void ensureDDL(Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("CREATE TABLE DOCTOR(id INT NOT NULL, name VARCHAR(255))");
+        preparedStatement.execute();
+    }
 
+    private static Connection connect() throws SQLException {
+        return DriverManager.getConnection("jdbc:h2:mem:default");
     }
 }
